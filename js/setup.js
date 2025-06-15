667b7e113e91ac40a9237f58de2c9ed229e28c47
ggorlen <gsgorlen@gmail.com>
2025-06-14 19:48:54 -0700

    Strip out server code and simplify

---
+++ js/setup.js
@@ -0,0 +1,52 @@
+var wallList = [];
+var hazardList = [];
+//
+function loadLevel(level) {
+  hazardList = [];
+  wallList = [];
+  for (var i = 0; i < level.hazards.length; i++) {
+    h = level.hazards[i];
+    pp = new paper.Path(h);
+    if (h.sm) pp.smooth();
+    hazardList.push(pp);
+  }
+
+  for (var i = 0; i < level.walls.length; i++) {
+    w = wall(level.walls[i]);
+    wallList.push(w);
+  }
+  hole = new paper.Point(
+    level.hole.x * (0.01 * window.innerWidth),
+    level.hole.y * (0.01 * window.innerHeight),
+  );
+  ball = circle(
+    level.ballStart.x * (0.01 * window.innerWidth),
+    level.ballStart.y * (0.01 * window.innerHeight),
+    8,
+    hole,
+  );
+  shots = new paper.PointText({
+    point: [window.innerWidth * 0.05, window.innerHeight * 0.1],
+    justification: "center",
+    fillColor: "white",
+    strokeColor: "black",
+    strokeWidth: 2,
+    fontSize: window.innerHeight * 0.1,
+    content: 0 + "/" + level.par,
+    "background-color": "white",
+  });
+  shots.content = 0 + "/" + level.par.textContent;
+
+  function wall(points) {
+    for (var i = 0; i < points.length; i++) {
+      points[i][0] = points[i][0] * (0.01 * window.innerWidth);
+      points[i][1] = points[i][1] * (0.01 * window.innerHeight);
+    }
+    w = new paper.Path({
+      segments: points,
+      strokeColor: "black",
+      strokeWidth: 8,
+    });
+    return w;
+  }
+}
